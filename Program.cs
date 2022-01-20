96e900f3b51cc7a3d0b5e5e0d4c354b713b51aa4
Bui-Huong <81941399+Bui-Huong@users.noreply.github.com>
2022-01-20 09:50:32 +0700

    day2

--- Program.cs
+++ Program.cs
@@ -12,10 +12,11 @@ namespace C__Assignment___Day_1
         {
             //bai 1
             MembersUntil membersUntil = new MembersUntil();
-            var maleMembers = membersUntil.Members.Where(m => m.Gender == Gender.Male);
-            var femaleMembers = membersUntil.Members.Where(m => m.Gender == Gender.Female);
-            var otherMembers = membersUntil.Members.Where(m => m.Gender == Gender.Other);
+            var maleMembers = membersUntil.GetAllMembers().Where(m => m.Gender == Gender.Male);
+            var femaleMembers = membersUntil.GetAllMembers().Where(m => m.Gender == Gender.Female);
+            var otherMembers = membersUntil.GetAllMembers().Where(m => m.Gender == Gender.Other);
             Console.WriteLine("Female list: ");
+            
             foreach (Member member in femaleMembers)
             {
                 Console.WriteLine("{0}", member.MemberInfo);
@@ -33,6 +34,8 @@ namespace C__Assignment___Day_1
                 Console.WriteLine("{0}", member.MemberInfo);
             }
             // bài 2
+            Member olderstMember = membersUntil.GetOldestMember(membersUntil.GetAllMembers());
+            Console.WriteLine("Oldest: {0}", olderstMember.MemberInfo);
             // var max_age = 0;
             // foreach (Member member in members)
             // {
@@ -49,114 +52,123 @@ namespace C__Assignment___Day_1
             //     }
             // }
             //bai 3
-            List<String> fullNames = membersUntil.FullNames(listMembers);
-            Console.WriteLine("Full name in new list: ");
-            fullNames.Foreach(m => { Console.WriteLine("Full name: " + m); });
-            // foreach (var people in members)
-            // {
-            //     newListMember.Add($"{people.FirstName} {people.LastName}");
-            // }
-            // foreach (string person in newListMember)
-            // {
-            //     Console.WriteLine(person);
-            // }
+            foreach(string name in membersUntil.GetFullNames(membersUntil.GetAllMembers()))
+            {
+                Console.WriteLine("Full name in new list: "+ name);
+            }
+            // List<String> fullNames = membersUntil.FullNames(listMembers);
+            // Console.WriteLine("Full name in new list: ");
+            // fullNames.Foreach(m => { Console.WriteLine("Full name: " + m); });
+            // // foreach (var people in members)
+            // // {
+            // //     newListMember.Add($"{people.FirstName} {people.LastName}");
+            // // }
+            // // foreach (string person in newListMember)
+            // // {
+            // //     Console.WriteLine(person);
+            // // }
             //bai 4
-            // Console.WriteLine("List of members who has birth year is 2000: ");
-            // foreach(Member member in members){
-            //     if(member.DateOfBirth.Year == 2000){
-            //         Console.WriteLine("{0} {1} {2}", member.FirstName, member.LastName, member.DateOfBirth);
-            //     }
-            // }
-            // Console.WriteLine("List of members who has birth year greater than 2000: ");
-            // foreach(Member member in members){
-            //     if(member.DateOfBirth.Year < 2000){
-            //         Console.WriteLine("{0} {1} {2}", member.FirstName, member.LastName, member.DateOfBirth);
-            //     }
-            // }
-            // Console.WriteLine("List of members who has birth year less than 2000: ");
-            // foreach(Member member in members){
-            //     if(member.DateOfBirth.Year > 2000){
-            //         Console.WriteLine("{0} {1} {2}", member.FirstName, member.LastName, member.DateOfBirth);
-            //     }
-            // }
-            // List<string> list1 = new List<string>();
-            // List<string> list2 = new List<string>();
-            // List<string> list3 = new List<string>();
-            // var T = 0;
-            // foreach (Member member in members)
-            // {
-            //     var birthYear = member.DateOfBirth.Year;
-            //     if (birthYear == 2000)
-            //     {
-            //         T = 0;
-            //     }
-            //     else if (birthYear > 2000)
-            //     {
-            //         T = 1;
-            //     }
-            //     else
-            //     {
-            //         T = 2;
-            //     }
-            //     switch (T)
-            //     {
-            //         case 0:
-            //             list1.Add($"{member.FirstName} {member.LastName} {member.Gender} {member.DateOfBirth} {member.PhoneNumber} {member.BirthPlace} {member.IsGraduated}");
-            //             break;
-            //         case 1:
-            //             list2.Add($"{member.FirstName} {member.LastName} {member.Gender} {member.DateOfBirth} {member.PhoneNumber} {member.BirthPlace} {member.IsGraduated}");
-            //             break;
-            //         case 2:
-            //             list3.Add($"{member.FirstName} {member.LastName} {member.Gender} {member.DateOfBirth} {member.PhoneNumber} {member.BirthPlace} {member.IsGraduated}");
-            //             break;
-            //     }
-            // }
-            // Console.WriteLine("--------------------------------------------------------------");
-            // Console.WriteLine("List of members who has birth year is 2000: ");
-            // foreach (string member in list1)
-            // {
-            //     Console.WriteLine(member);
-            // }
-            // Console.WriteLine("--------------------------------------------------------------");
-            // Console.WriteLine("List of members who has birth year greater than 2000: ");
-            // foreach (string member in list2)
-            // {
-            //     Console.WriteLine(member);
-            // }
-            // Console.WriteLine("--------------------------------------------------------------");
-            // Console.WriteLine("List of members who has birth year less than 2000:  ");
-            // foreach (string member in list3)
-            // {
-            //     Console.WriteLine(member);
-            // }
-            // Console.WriteLine("--------------------------------------------------------------");
+            foreach(Member m in membersUntil.GetMembersByBirthYearEqual2000(membersUntil.GetAllMembers()))
+            {
+                Console.WriteLine("Members By Birth Year Equal 2000: " +m.MemberInfo);
+            }
+            membersUntil.GetMembersByBirthYearGreatThan2000(membersUntil.GetAllMembers()).ForEach(m=>Console.WriteLine("Members By Birth Year Great Than 2000: "+m.MemberInfo));
+            // // Console.WriteLine("List of members who has birth year is 2000: ");
+            // // foreach(Member member in members){
+            // //     if(member.DateOfBirth.Year == 2000){
+            // //         Console.WriteLine("{0} {1} {2}", member.FirstName, member.LastName, member.DateOfBirth);
+            // //     }
+            // // }
+            // // Console.WriteLine("List of members who has birth year greater than 2000: ");
+            // // foreach(Member member in members){
+            // //     if(member.DateOfBirth.Year < 2000){
+            // //         Console.WriteLine("{0} {1} {2}", member.FirstName, member.LastName, member.DateOfBirth);
+            // //     }
+            // // }
+            // // Console.WriteLine("List of members who has birth year less than 2000: ");
+            // // foreach(Member member in members){
+            // //     if(member.DateOfBirth.Year > 2000){
+            // //         Console.WriteLine("{0} {1} {2}", member.FirstName, member.LastName, member.DateOfBirth);
+            // //     }
+            // // }
+            // // List<string> list1 = new List<string>();
+            // // List<string> list2 = new List<string>();
+            // // List<string> list3 = new List<string>();
+            // // var T = 0;
+            // // foreach (Member member in members)
+            // // {
+            // //     var birthYear = member.DateOfBirth.Year;
+            // //     if (birthYear == 2000)
+            // //     {
+            // //         T = 0;
+            // //     }
+            // //     else if (birthYear > 2000)
+            // //     {
+            // //         T = 1;
+            // //     }
+            // //     else
+            // //     {
+            // //         T = 2;
+            // //     }
+            // //     switch (T)
+            // //     {
+            // //         case 0:
+            // //             list1.Add($"{member.FirstName} {member.LastName} {member.Gender} {member.DateOfBirth} {member.PhoneNumber} {member.BirthPlace} {member.IsGraduated}");
+            // //             break;
+            // //         case 1:
+            // //             list2.Add($"{member.FirstName} {member.LastName} {member.Gender} {member.DateOfBirth} {member.PhoneNumber} {member.BirthPlace} {member.IsGraduated}");
+            // //             break;
+            // //         case 2:
+            // //             list3.Add($"{member.FirstName} {member.LastName} {member.Gender} {member.DateOfBirth} {member.PhoneNumber} {member.BirthPlace} {member.IsGraduated}");
+            // //             break;
+            // //     }
+            // // }
+            // // Console.WriteLine("--------------------------------------------------------------");
+            // // Console.WriteLine("List of members who has birth year is 2000: ");
+            // // foreach (string member in list1)
+            // // {
+            // //     Console.WriteLine(member);
+            // // }
+            // // Console.WriteLine("--------------------------------------------------------------");
+            // // Console.WriteLine("List of members who has birth year greater than 2000: ");
+            // // foreach (string member in list2)
+            // // {
+            // //     Console.WriteLine(member);
+            // // }
+            // // Console.WriteLine("--------------------------------------------------------------");
+            // // Console.WriteLine("List of members who has birth year less than 2000:  ");
+            // // foreach (string member in list3)
+            // // {
+            // //     Console.WriteLine(member);
+            // // }
+            // // Console.WriteLine("--------------------------------------------------------------");
             //bai 5
-            // Console.WriteLine("Return the first person who was born in Ha Noi.");
-            // foreach (Member member in members)
-            // {
-            //     while (member.BirthPlace == "Ha Noi")
-            //     {
-            //         Console.WriteLine("{0} {1} {2} {3}", member.FirstName, member.LastName, member.Age, member.BirthPlace);
-            //     }
-            //     break;
-            // }
-            // Console.WriteLine("Return the first person who was born in Ha Noi.");
-            // int i = 0;
-            // while (true)
-            // {
-            //     if (members[i].BirthPlace == "Ha Noi")
-            //     {
-            //         Console.WriteLine("{0} {1} {2} {3}", members[i].FirstName, members[i].LastName, members[i].Age, members[i].BirthPlace);
-            //         break;
-            //     }
-            //     i++;
-            // }
-            Console.WriteLine("List of members who has birth less than 2000: ");
-            membersUntil.Members.Where(m->m.DateOfBirth.Year < 2000).Select(x->x.MemberInfo).ToList().Foreach(x->Console.WriteLine(x));
-            Console.WriteLine("List of members who has birth greater than 2000: ");
-            membersUntil.Members.Where(m->m.DateOfBirth.Year > 2000).Select(x->x.MemberInfo).ToList().Foreach(x->Console.WriteLine(x));
-            Console.WriteLine("List of members who has birth year is 2000: ");
-            membersUntil.Members.Where(m->m.DateOfBirth.Year == 2000).Select(x->x.MemberInfo).ToList().Foreach(x->Console.WriteLine(x));
+            Console.WriteLine("Return the first person who was born in Ha Noi: "+membersUntil.GetFirstMemberByBirthPlace(membersUntil.GetAllMembers(),"Ha Noi").MemberInfo);
+            // // foreach (Member member in members)
+            // // {
+            // //     while (member.BirthPlace == "Ha Noi")
+            // //     {
+            // //         Console.WriteLine("{0} {1} {2} {3}", member.FirstName, member.LastName, member.Age, member.BirthPlace);
+            // //     }
+            // //     break;
+            // // }
+            // // Console.WriteLine("Return the first person who was born in Ha Noi.");
+            // // int i = 0;
+            // // while (true)
+            // // {
+            // //     if (members[i].BirthPlace == "Ha Noi")
+            // //     {
+            // //         Console.WriteLine("{0} {1} {2} {3}", members[i].FirstName, members[i].LastName, members[i].Age, members[i].BirthPlace);
+            // //         break;
+            // //     }
+            // //     i++;
+            // // }
+            // Console.WriteLine("List of members who has birth less than 2000: ");
+            // membersUntil.Members.Where(m->m.DateOfBirth.Year < 2000).Select(x->x.MemberInfo).ToList().Foreach(x->Console.WriteLine(x));
+            // Console.WriteLine("List of members who has birth greater than 2000: ");
+            // membersUntil.Members.Where(m->m.DateOfBirth.Year > 2000).Select(x->x.MemberInfo).ToList().Foreach(x->Console.WriteLine(x));
+            // Console.WriteLine("List of members who has birth year is 2000: ");
+            // membersUntil.Members.Where(m->m.DateOfBirth.Year == 2000).Select(x->x.MemberInfo).ToList().Foreach(x->Console.WriteLine(x));
 
         }
     }
--- Untils/MembersUntil.cs
+++ Untils/MembersUntil.cs
@@ -7,10 +7,10 @@ namespace C__Assignment___Day_1.Untils
 {
     public class MembersUntil
     {
-        public List<Member> Members;
+        private List<Member> members;
         public MembersUntil()
         {
-            Members = InitMembers();
+            members = InitMembers();
         }
         private List<Member> InitMembers()
         {
@@ -44,18 +44,33 @@ namespace C__Assignment___Day_1.Untils
                 },
             };
         }
-        public Member GetOldestMember(List<Member> members)
+        public Member GetOldestMember(List<Member> abc)
         {
             return members.Where(m => m.Age == members.Max(m=>m.Age)).FirstOrDefault();
         }
-        public List<string> FullNames(List<Member> members)
+        public List<string> GetFullNames(List<Member> members)
         {
             return members.Select(mem=>mem.MemberInfo).ToList();
         }
-        public Member GetFirstMemberByBirthPlace(string birthPlace)
+        public Member GetFirstMemberByBirthPlace(List<Member> members,string birthPlace)
         {
-            Member result = _members.First(member=> member.BirthPlace == birthPlace);
+            Member result = members.FirstOrDefault(member=> member.BirthPlace == birthPlace);
             return result;
+        }
+        public List<Member> GetAllMembers()
+        {
+            return members;
+        }
+        public List<Member> GetMembersByBirthYearEqual2000(List<Member> members)
+        {
+            return members.Where(m => m.DateOfBirth.Year == 2000).ToList();
+        }
+        public List<Member> GetMembersByBirthYearLessThan2000(List<Member> members)
+        {
+            return members.Where(m => m.DateOfBirth.Year < 2000).ToList();
+        }        public List<Member> GetMembersByBirthYearGreatThan2000(List<Member> members)
+        {
+            return members.Where(m => m.DateOfBirth.Year > 2000).ToList();
         }
     }
 }
